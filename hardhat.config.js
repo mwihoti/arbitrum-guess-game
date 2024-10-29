72c2fb11e45c91866044b3d9ba1641b5754c3058
mwihoti <danielmwihoti@gmail.com>
2024-10-29 15:49:04 +0300

    hardhat configuration - added etherscan api key

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,11 @@
+const { arbitrumSepolia } = require("viem/chains");
+
 require("@nomicfoundation/hardhat-toolbox");
 require('dotenv').config();
 
 const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
+const ARBITRUM_SEPOLIA_RPC_URL = process.env.ARBITRUM_SEPOLIA_RPC_URL;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
 
 
 /** @type import('hardhat/config').HardhatUserConfig */
@@ -13,5 +17,21 @@ module.exports = {
       chainId: 421614,
       accounts: [PRIVATE_KEY]
     }
+  },
+
+  etherscan: {
+    apiKey: {
+      arbitrumSepolia: ETHERSCAN_API_KEY,
+    },
+    customChains: [
+      {
+        network: "arbitrumSepolia",
+        chainId: 421614,
+        urls: {
+          apiURL: "https://api-sepolia.arbiscan.io/api",
+          browserURL: "https://sepolia.arbiscan.io/",
+        }
+      }
+    ]
   }
 };
