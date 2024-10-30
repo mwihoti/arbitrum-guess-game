1204048efdd1cc8d66dcf095e0422930afa49ecf
mwihoti <danielmwihoti@gmail.com>
2024-10-30 13:19:12 +0300

    changed prize transfer to point System only

--- contracts/GuessGame.sol
+++ contracts/GuessGame.sol
@@ -7,10 +7,15 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract GuessGame is Ownable {
     uint256 private number = 5;
     uint256 public betAmount = 0.00000001 ether;
-    uint256 public prize = 0.0000005 ether;
+    //uint256 public prize = 0.0000005 ether;
+
+
+    // mapping to track each player's points
+    mapping(address => uint256) public playerPoints;
 
 
     event NumberGuessed(address player, uint256 guessedNumber, bool won);
+    event PointsAwarded(address player, uint256 points);
 
     // constructor to set the owner of the contract
 
@@ -25,13 +30,22 @@ contract GuessGame is Ownable {
         require(msg.value == betAmount, "Incorrect bet amount");
         require(_guess > 0 && _guess <= 10, "Number must be between 1 and 10");
 
+        // Award 5 points for making a guess
+        playerPoints[msg.sender] += 5;
+
         bool won = (_guess == number);
 
         if (won) {
-            payable(msg.sender).transfer(prize);
+            // Award an addittional 15 points for a correct guess 
+            playerPoints[msg.sender] += 15;
         }
 
         emit NumberGuessed(msg.sender, _guess, won);
+        emit PointsAwarded(msg.sender, playerPoints[msg.sender]);
+    }
+
+    function getPoints(address _player) external view returns (uint256) {
+        return playerPoints[_player];
     }
 
     function withdraw() external onlyOwner {
